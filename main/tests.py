2ad6b740a236a604c54768981ca177cea4435089
00013290 <wiut13290@gmail.com>
2026-03-06 23:56:30 +0500

    Add tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,76 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Post, Category, Comment
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name='Test Category',
+            description='Test Description'
+        )
+
+    def test_category_created(self):
+        self.assertEqual(self.category.name, 'Test Category')
+
+    def test_category_str(self):
+        self.assertEqual(str(self.category), 'Test Category')
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.category = Category.objects.create(name='Tech')
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='Test Content',
+            author=self.user
+        )
+        self.post.categories.add(self.category)
+
+    def test_post_created(self):
+        self.assertEqual(self.post.title, 'Test Post')
+
+    def test_post_author(self):
+        self.assertEqual(self.post.author, self.user)
+
+    def test_post_str(self):
+        self.assertEqual(str(self.post), 'Test Post')
+
+
+class ViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='Test Content',
+            author=self.user
+        )
+
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail(self):
+        response = self.client.get(reverse('post_detail', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_required_for_post_create(self):
+        response = self.client.get(reverse('post_create'))
+        self.assertEqual(response.status_code, 302)
